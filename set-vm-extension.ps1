2c69a0d5228020dee42e60807c9a6d44db74fd27
Robert Zhang <louirobert@gmail.com>
2019-03-22 11:00:53 +0800

    Log the process of installation on VM.

--- set-vm-extension.ps1
+++ set-vm-extension.ps1
@@ -7,11 +7,15 @@ param(
     [string] $packageUrl
 )
 
-$vm = Get-AzVM -ResourceGroupName $vmResourceGroup -Name $vmName
+# FIXME: The vmelog_xxx is almost "empty", while it's OK in an interactive shell.
+$script = "curl -L https://raw.githubusercontent.com/coin8086/node-manager-deployer/master/start | bash -s -- $packageUrl > /var/log/vmelog_`$(date -Ins)"
+"Command to run:`n$script`n" | Out-Default
 
-$script = "curl -L https://raw.githubusercontent.com/coin8086/node-manager-deployer/master/start | bash -s -- $packageUrl"
 $bytes = [System.Text.Encoding]::UTF8.GetBytes($script)
 $b64s = [Convert]::ToBase64String($Bytes)
 $settings = @{ script = $b64s }
 
+$vm = Get-AzVM -ResourceGroupName $vmResourceGroup -Name $vmName
+
+"Setting VM Extension..." | Out-Default
 Set-AzVMExtension -ResourceGroupName $vm.ResourceGroupName -VMName $vm.Name -Location $vm.Location -ExtensionType CustomScript -Publisher Microsoft.Azure.Extensions -TypeHandlerVersion 2.0 -Name VME -Settings $settings

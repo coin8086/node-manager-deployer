f21bf7870cf64a0490bb903223a8a1a54a7a7606
Robert Zhang <louirobert@gmail.com>
2019-03-25 17:52:52 +0800

    Modify set-vm-extension.ps1 to support Windows VM, and add unset-vm-extension.ps1

--- unset-vm-extension.ps1
+++ unset-vm-extension.ps1
@@ -2,9 +2,7 @@ param(
     [Parameter(Mandatory = $true)]
     [string] $vmResourceGroup,
     [Parameter(Mandatory = $true)]
-    [string] $vmName,
-    [Parameter(Mandatory = $true)]
-    [string] $packageUrl
+    [string] $vmName
 )
 
 $vm = Get-AzVM -ResourceGroupName $vmResourceGroup -Name $vmName
@@ -13,18 +11,18 @@ $vm | Out-Default
 if ($vm.OSProfile.LinuxConfiguration) {
     $settings = @{
         timestamp = $(Get-Date).Ticks
-        fileUris = @('https://raw.githubusercontent.com/coin8086/node-manager-deployer/master/install.sh')
-        commandToExecute = "./install.sh $packageUrl"
+        fileUris = @('https://raw.githubusercontent.com/coin8086/node-manager-deployer/master/uninstall.sh')
+        commandToExecute = "./uninstall.sh"
         skipDos2Unix = $true
     }
-    "Setting VM Extension..." | Out-Default
+    "Unsetting VM Extension..." | Out-Default
     Set-AzVMExtension -ResourceGroupName $vm.ResourceGroupName -VMName $vm.Name -Location $vm.Location -ExtensionType CustomScript -Publisher Microsoft.Azure.Extensions -TypeHandlerVersion 2.0 -Name VME -Settings $settings
 }
 else {
     $settings = @{
         timestamp = $(Get-Date).Ticks
-        fileUris = @('https://raw.githubusercontent.com/coin8086/node-manager-deployer/master/install.ps1')
-        commandToExecute = "powershell -ExecutionPolicy Unrestricted -File install.ps1 $packageUrl"
+        fileUris = @('https://raw.githubusercontent.com/coin8086/node-manager-deployer/master/uninstall.ps1')
+        commandToExecute = "powershell -ExecutionPolicy Unrestricted -File uninstall.ps1"
     }
     "Setting VM Extension..." | Out-Default
     Set-AzVMExtension -ResourceGroupName $vm.ResourceGroupName -VMName $vm.Name -Location $vm.Location -ExtensionType CustomScriptExtension -Publisher Microsoft.Compute -TypeHandlerVersion 1.9 -Name VME -Settings $settings
